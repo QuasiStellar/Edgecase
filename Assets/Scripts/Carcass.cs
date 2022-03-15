9dcc6aa3aa9141c0fb4281f05c260b40f4b4c76f
ANTIKILLER <92469820+zazamrykh@users.noreply.github.com>
2022-03-16 00:52:58 +0300

    #5 Make surface into one mesh (#9)

---
+++ Assets/Scripts/Carcass.cs
@@ -0,0 +1,15 @@
+﻿public class Carcass
+{
+    private const int MeshSize = MapGenerator.MapSize;
+    private readonly int[,] _heights = new int[MeshSize * 2 - 1, MeshSize * 2 - 1];
+
+    public void SetHeight(int aPos, int bPos, int height)
+    {
+        _heights[aPos + MeshSize - 1, bPos + MeshSize - 1] = height;
+    }
+
+    public int GetHeight(int aPos, int bPos)
+    {
+        return _heights[aPos + MeshSize - 1, bPos + MeshSize - 1];
+    }
+}
