0402d584f39b3931213563a9e9f7db57297194f2
Zakhar Bastard <zaxar0.0.0@mail.ru>
2022-03-16 00:49:09 +0300

    Make surface into one mesh

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map
+{
+    private readonly GameObject _hexGameObject;
+
+    private const float HexSize = MapGenerator.HexSize;
+    private const int MapSize = MapGenerator.MapSize;
+    private const int TriangleSize = MapGenerator.TriangleSize;
+
+    private const float Sqrt3By2 = 0.866025403784439f; // Mathf.Pow(3, 0.5f) / 2
+    private const float Sqrt3By4 = 0.433012701892219f; // Mathf.Pow(3, 0.5f) / 4
+
+    private const int StairHeight = 3;
+    private const int HeightUnderZero = 0;
+
+    private const int LowerY = HeightUnderZero * StairHeight;
+
+    private static readonly Dictionary<int, Color> ColorMap = new Dictionary<int, Color>
+    {
+        {0, new Color(1, 0, 0)},
+        {1, new Color(1, 0.5f, 0)},
+        {2, new Color(1, 1, 0)},
+        {3, new Color(0, 1, 0)},
+        {4, new Color(0, 1, 1)},
+        {5, new Color(0, 0, 1)},
+        {6, new Color(1, 0, 1)}
+    };
+
+    public Map(Carcass carcass)
+    {
+        _hexGameObject = new GameObject("Hex");
+        _hexGameObject.AddComponent<MeshRenderer>().material = Resources.Load<Material>("HexMaterial");
+
+        var verticesList = new List<Vector3>();
+        var trianglesList = new List<int>();
+        var uvList = new List<Vector2>();
+        var verticesCounter = 0;
+
+        // Creating hexes
+        for (var i = 0; i < 2 * MapSize - 1; i++)
+        {
+            for (var j = 0; j < 2 * MapSize - 1; j++)
+            {
+                if (Math.Abs(i - j) >= MapSize) continue;
+
+                var aPos = i - MapSize + 1;
+                var bPos = j - MapSize + 1;
+
+                var height = carcass.GetHeight(aPos, bPos);
+                var upperY = height * StairHeight;
+
+                var coordinateShiftAPos = (aPos - bPos / 2f) * HexSize * Sqrt3By2;
+                var coordinateShiftBPos = bPos * HexSize * 3 / 4;
+                coordinateShiftAPos += aPos * TriangleSize;
+                coordinateShiftBPos += bPos * TriangleSize * Sqrt3By2;
+                coordinateShiftAPos -= bPos * (TriangleSize / 2f);
+
+                verticesList.AddRange(new[]
+                {
+                    new Vector3(coordinateShiftAPos + 0, upperY,
+                        coordinateShiftBPos + HexSize / 2),
+                    new Vector3(coordinateShiftAPos + HexSize * Sqrt3By4, upperY,
+                        coordinateShiftBPos + HexSize / 4),
+                    new Vector3(coordinateShiftAPos + HexSize * Sqrt3By4, upperY,
+                        coordinateShiftBPos - HexSize / 4),
+                    new Vector3(coordinateShiftAPos + 0, upperY,
+                        coordinateShiftBPos - HexSize / 2),
+                    new Vector3(coordinateShiftAPos - HexSize * Sqrt3By4, upperY,
+                        coordinateShiftBPos - HexSize / 4),
+                    new Vector3(coordinateShiftAPos - HexSize * Sqrt3By4, upperY,
+                        coordinateShiftBPos + HexSize / 4)
+                });
+
+                trianglesList.AddRange(new[]
+                {
+                    0 + verticesCounter, 1 + verticesCounter, 2 + verticesCounter,
+                    0 + verticesCounter, 2 + verticesCounter, 3 + verticesCounter,
+                    0 + verticesCounter, 3 + verticesCounter, 5 + verticesCounter,
+                    3 + verticesCounter, 4 + verticesCounter, 5 + verticesCounter
+                });
+
+                uvList.AddRange(new[]
+                {
+                    new Vector2(0.5f, 1 / 6f + 0.01f),
+                    new Vector2(0.75f - 0.01f, 1 / 3f + 0.01f),
+                    new Vector2(0.75f - 0.01f, 2 / 3f - 0.01f),
+                    new Vector2(0.5f, 5 / 6f - 0.01f),
+                    new Vector2(0.25f + 0.01f, 2 / 3f - 0.01f),
+                    new Vector2(0.25f + 0.01f, 1 / 3f + 0.01f)
+                });
+
+                verticesCounter += 6;
+            }
+        }
+
+        // Creating triangles between hexes
+        var hexCounter = 0;
+        for (var i = 0; i < 2 * MapSize - 1; i++)
+        {
+            for (var j = 0; j < 2 * MapSize - 1; j++)
+            {
+                if (Math.Abs(i - j) >= MapSize) continue;
+                var verticesShiftGrowing = (MapSize + 1) * 6 + (hexCounter + i) * 6;
+                var verticesShiftDecreasing = (MapSize + 1) * 6 + (hexCounter - i + (MapSize * 2 - 3)) * 6;
+
+                // Creating up-down triangles
+                if (j + 1 < 2 * MapSize - 1 && i < 2 * MapSize - 2 && Math.Abs(i - (j + 1)) < MapSize)
+                {
+                    if (i < MapSize - 1)
+                        trianglesList.AddRange(new[]
+                        {
+                            0 + hexCounter * 6,
+                            8 + hexCounter * 6,
+                            4 + verticesShiftGrowing
+                        });
+                    else
+                        trianglesList.AddRange(new[]
+                        {
+                            0 + hexCounter * 6,
+                            8 + hexCounter * 6,
+                            4 + verticesShiftDecreasing
+                        });
+                }
+
+                // Creating upright triangles
+                if (j + 1 < 2 * MapSize - 1 && i < 2 * MapSize - 2 && Math.Abs((i + 1) - j) < MapSize)
+                {
+                    if (i < MapSize - 1)
+                        trianglesList.AddRange(new[]
+                        {
+                            1 + hexCounter * 6,
+                            3 + verticesShiftGrowing,
+                            -1 + verticesShiftGrowing
+                        });
+                    else
+                        trianglesList.AddRange(new[]
+                        {
+                            1 + hexCounter * 6,
+                            3 + verticesShiftDecreasing,
+                            -1 + verticesShiftDecreasing
+                        });
+                }
+
+                // Creating quadrilateral
+                if (j + 1 < 2 * MapSize - 1 && Math.Abs(i - (j + 1)) < MapSize)
+                {
+                    trianglesList.AddRange(new[]
+                    {
+                        0 + hexCounter * 6,
+                        9 + hexCounter * 6,
+                        8 + hexCounter * 6,
+                        5 + hexCounter * 6,
+                        9 + hexCounter * 6,
+                        0 + hexCounter * 6
+                    });
+                }
+
+                if (j + 1 < 2 * MapSize - 1 && i + 1 < 2 * MapSize - 1)
+                {
+                    if (i < MapSize - 1)
+                        trianglesList.AddRange(new[]
+                        {
+                            1 + hexCounter * 6,
+                            0 + hexCounter * 6,
+                            4 + verticesShiftGrowing,
+                            4 + verticesShiftGrowing,
+                            3 + verticesShiftGrowing,
+                            1 + hexCounter * 6
+                        });
+                    else
+                        trianglesList.AddRange(new[]
+                        {
+                            1 + hexCounter * 6,
+                            0 + hexCounter * 6,
+                            4 + verticesShiftDecreasing,
+                            4 + verticesShiftDecreasing,
+                            3 + verticesShiftDecreasing,
+                            1 + hexCounter * 6
+                        });
+                }
+
+                if (i < 2 * MapSize - 2 && Math.Abs((i + 1) - j) < MapSize)
+                {
+                    if (i < MapSize - 1)
+                        trianglesList.AddRange(new[]
+                        {
+                            1 + hexCounter * 6,
+                            5 + verticesShiftGrowing - 6,
+                            2 + hexCounter * 6,
+                            2 + hexCounter * 6,
+                            5 + verticesShiftGrowing - 6,
+                            4 + verticesShiftGrowing - 6
+                        });
+                    else
+                        trianglesList.AddRange(new[]
+                        {
+                            1 + hexCounter * 6,
+                            5 + verticesShiftDecreasing - 6,
+                            2 + hexCounter * 6,
+                            2 + hexCounter * 6,
+                            5 + verticesShiftDecreasing - 6,
+                            4 + verticesShiftDecreasing - 6
+                        });
+                }
+
+                hexCounter++;
+            }
+        }
+
+        _hexGameObject.AddComponent<MeshFilter>().mesh = new Mesh
+        {
+            vertices = verticesList.ToArray(),
+            triangles = trianglesList.ToArray(),
+            uv = uvList.ToArray(),
+        };
+
+        _hexGameObject.GetComponent<MeshRenderer>().material.color = ColorMap[3];
+    }
+}
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,19 +1,25 @@
 using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
+
+
 public class MapGenerator : MonoBehaviour
 {
-    private const int MapSize = 10;
-    private const float Smoothness = 10f;
+    public const int MapSize = 5;
+    public const int TriangleSize = 5;
+    public const float HexSize = 10;
+
+    private const float Smoothness = 7f;
     private const int HeightVariation = 7;
     private static int _noiseShift;
+
     public GameObject freeCamera;
     public GameObject topDownCamera;
 
     private void Start()
     {
-        var map = new Hex[MapSize * 2 - 1, MapSize * 2 - 1];
-        _noiseShift = Random.Range(10000,100000);
+        var mesh = new Carcass();
+        _noiseShift = Random.Range(10000, 100000);
         for (var i = 0; i < MapSize * 2 - 1; i++)
         {
             for (var j = 0; j < MapSize * 2 - 1; j++)
@@ -21,18 +27,14 @@ public class MapGenerator : MonoBehaviour
                 if (Math.Abs(i - j) >= MapSize) continue;
                 int aPos = i - MapSize + 1;
                 int bPos = j - MapSize + 1;
-                int height = (int) (Mathf.PerlinNoise((aPos/Smoothness) + _noiseShift, 
-                    (bPos/Smoothness) + _noiseShift) * HeightVariation);
-                var hex = new Hex(
-                    aPos, bPos, height
-                );
-                hex.SetParent(transform);
-                hex.UpdateColor();
-                map[i, j] = hex;
+                mesh.SetHeight(aPos, bPos, (int) (Mathf.PerlinNoise((aPos / Smoothness) + _noiseShift,
+                    (bPos / Smoothness) + _noiseShift) * HeightVariation));
             }
         }
 
-        freeCamera.transform.position = new Vector3(0, MapSize * Hex.HexSize, - MapSize * Hex.HexSize * 0.75f);
-        topDownCamera.GetComponent<Camera>().orthographicSize = MapSize * Hex.HexSize * 0.8f;
+        var map = new Map(mesh);
+
+        freeCamera.transform.position = new Vector3(0, MapSize * HexSize, -MapSize * HexSize * 0.75f);
+        topDownCamera.GetComponent<Camera>().orthographicSize = MapSize * HexSize * 0.8f;
     }
 }
