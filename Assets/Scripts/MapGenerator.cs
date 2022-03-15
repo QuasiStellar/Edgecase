a2cf89f1b773e89a2955b6c2e26865987bced98a
ANTIKILLER <92469820+zazamrykh@users.noreply.github.com>
2022-03-16 00:52:58 +0300

    5 Make surface into one mesh (#9)

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,29 +1,34 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 
 public class MapGenerator : MonoBehaviour
 {
-    private const int MapSize = 10;
+    public const int MapSize = 5;
+    public const int TriangleSize = 5;
+    public const float HexSize = 10;
+
+    private const float Smoothness = 7f;
+    private const int HeightVariation = 7;
+    private static int _noiseShift;
 
     public GameObject freeCamera;
     public GameObject topDownCamera;
 
     private void Start()
     {
-        var map = new Hex[MapSize * 2 - 1, MapSize * 2 - 1];
+        var mesh = new Carcass();
+        _noiseShift = Random.Range(10000, 100000);
         for (var i = 0; i < MapSize * 2 - 1; i++)
         {
             for (var j = 0; j < MapSize * 2 - 1; j++)
             {
                 if (Math.Abs(i - j) >= MapSize) continue;
-                var hex = new Hex(
-                    i - MapSize + 1,
-                    j - MapSize + 1,
-                    Hex.GetRandomHeight()
-                );
-                hex.SetParent(transform);
-                hex.UpdateColor();
-                map[i, j] = hex;
+                int aPos = i - MapSize + 1;
+                int bPos = j - MapSize + 1;
+                mesh.SetHeight(aPos, bPos, (int) (Mathf.PerlinNoise((aPos / Smoothness) + _noiseShift,
+                    (bPos / Smoothness) + _noiseShift) * HeightVariation));
             }
         }
 
