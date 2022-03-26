8ba8e364f3f97f8d1d0facc087436dc171fd0ae8
Zakhar Bastard <zaxar0.0.0@mail.ru>
2022-03-27 02:02:08 +0300

    Implement uniform horizontal lifting of all mesh

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -8,14 +9,15 @@ public class MapGenerator : MonoBehaviour
     public const int MapSize = 5;
     public const int TriangleSize = 5;
     public const float HexSize = 10;
+    public const int StairHeight = 3;
 
-    private const float Smoothness = 7f;
-    private const int HeightVariation = 7;
+    public const float Smoothness = 7f;
+    public const int HeightVariation = 7;
     private static int _noiseShift;
 
     public GameObject freeCamera;
     public GameObject topDownCamera;
-
+    public Map map;
     private void Start()
     {
         var mesh = new Carcass();
@@ -25,16 +27,39 @@ public class MapGenerator : MonoBehaviour
             for (var j = 0; j < MapSize * 2 - 1; j++)
             {
                 if (Math.Abs(i - j) >= MapSize) continue;
-                int aPos = i - MapSize + 1;
-                int bPos = j - MapSize + 1;
+                var aPos = i - MapSize + 1;
+                var bPos = j - MapSize + 1;
                 mesh.SetHeight(aPos, bPos, (int) (Mathf.PerlinNoise((aPos / Smoothness) + _noiseShift,
                     (bPos / Smoothness) + _noiseShift) * HeightVariation));
             }
         }
-
-        var map = new Map(mesh);
+        map = new Map(mesh);
 
         freeCamera.transform.position = new Vector3(0, MapSize * HexSize, -MapSize * HexSize * 0.75f);
         topDownCamera.GetComponent<Camera>().orthographicSize = MapSize * HexSize * 0.8f;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Elevate());
+        }
+    }
+    private IEnumerator Elevate()
+    {
+        var elapsedTime = 0f;
+        while (elapsedTime < 1)
+        {
+            for (var i = 0; i < MapSize * 2 - 1; i++)
+            {
+                for (var j = 0; j < MapSize * 2 - 1; j++)
+                {
+                    if (Math.Abs(i - j) >= MapSize) continue;
+                    map._hexGameObject.transform.Translate(Vector3.up * StairHeight  * Time.deltaTime);
+                }
+            }
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
