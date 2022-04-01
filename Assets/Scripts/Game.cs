b10715a8961014b83b95d6b3dec9f66aa1a9c862
QuasiStellar <quasistellar.official@gmail.com>
2022-04-02 01:44:01 +0300

    Move map generation to HexMapGenerator

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -5,40 +5,21 @@ using Random = UnityEngine.Random;
 
 public class Game : MonoBehaviour
 {
-    public const int MapSize = 5;
+    public const int MapSize = 10;
     public const float HexSize = 10;
-    public const int StairHeight = 3;
+    public const int StairHeight = 8;
 
-    public const float Smoothness = 7f;
-    public const int HeightVariation = 7;
-
-    private static int _noiseShift;
+    private const float Smoothness = 7f;
+    private const int HeightVariation = 7;
 
     public GameObject freeCamera;
     public GameObject topDownCamera;
 
+    private HexMap hexMap;
+
     private void Start()
     {
-        var mesh = new Carcass();
-        _noiseShift = Random.Range(10000, 100000);
-        for (var i = 0; i < MapSize * 2 - 1; i++)
-        {
-            for (var j = 0; j < MapSize * 2 - 1; j++)
-            {
-                if (Math.Abs(i - j) >= MapSize) continue;
-                var aPos = i - MapSize + 1;
-                var bPos = j - MapSize + 1;
-                var height = (int) (Mathf.PerlinNoise((aPos / Smoothness) + _noiseShift,
-                    (bPos / Smoothness) + _noiseShift) * HeightVariation);
-                if (height >= HeightVariation)
-                    height = HeightVariation - 1;
-                else if (height < 0)
-                    height = 0;
-                mesh.SetHeight(aPos, bPos, height);
-            }
-        }
-
-        var map = new HexMap(mesh);
+        hexMap = HexMapGenerator.GenerateMap(MapSize, Smoothness, HeightVariation);
 
         freeCamera.transform.position = new Vector3(0, MapSize * HexSize, -MapSize * HexSize * 0.75f);
         topDownCamera.GetComponent<Camera>().orthographicSize = MapSize * HexSize * 0.8f;
