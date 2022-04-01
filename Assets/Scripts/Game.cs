25266350f6142dd83406d068894c82ef5215514d
QuasiStellar <quasistellar.official@gmail.com>
2022-04-02 02:05:12 +0300

    Move all map generation logic to HexMapGenerator

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,15 +1,13 @@
-using System;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 
 public class Game : MonoBehaviour
 {
-    public const int MapSize = 10;
-    public const float HexSize = 10;
-    public const int StairHeight = 8;
+    private const int MapSize = 10;
+    private const int HexSize = 10;
+    private const int StairHeight = 8;
 
-    private const float Smoothness = 7f;
+    private const int Smoothness = 7;
     private const int HeightVariation = 7;
 
     public GameObject freeCamera;
@@ -19,7 +17,13 @@ public class Game : MonoBehaviour
 
     private void Start()
     {
-        hexMap = HexMapGenerator.GenerateMap(MapSize, Smoothness, HeightVariation);
+        hexMap = new HexMap(HexMapGenerator.GenerateMapGameObject(
+            MapSize,
+            Smoothness,
+            HeightVariation,
+            HexSize,
+            StairHeight
+        ));
 
         freeCamera.transform.position = new Vector3(0, MapSize * HexSize, -MapSize * HexSize * 0.75f);
         topDownCamera.GetComponent<Camera>().orthographicSize = MapSize * HexSize * 0.8f;
