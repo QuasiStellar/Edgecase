8ea0a8141aa1cfbccf61d2d1b4f70337fdee5f67
QuasiStellar <quasistellar.official@gmail.com>
2022-04-02 21:19:34 +0300

    Implement height map generator inheritance

---
+++ Assets/Scripts/FlatHeightMapGenerator.cs
@@ -0,0 +1,7 @@
+﻿public class FlatHeightMapGenerator : HeightMapGenerator
+{
+    public override int[,] HeightMap(int mapSize, float smoothness, int heightVariation)
+    {
+        return new int[mapSize * 2 - 1, mapSize * 2 - 1];
+    }
+}
