7b3cb6e6bd0c2b327811240bef19bd4ea5dcef7e
FridayItsFridayFriday <104296277+FridayItsFridayFriday@users.noreply.github.com>
2022-05-10 14:36:58 +0000

    Replace FlatHeightMap with FlatHeightMapGenerator

--- Assets/Scripts/Kernel/HeightMaps/FlatHeightMapGenerator.cs
+++ Assets/Scripts/Kernel/HeightMaps/FlatHeightMapGenerator.cs
@@ -3,19 +3,28 @@ using Utils;
 
 ﻿namespace Kernel.HeightMaps
 {
-    public class FlatHeightMap : HeightMap
+    public class FlatHeightMapGenerator
     {
-        public FlatHeightMap(int mapSize) : base(mapSize)
+        private readonly int _height;
+
+        public FlatHeightMapGenerator(int height)
         {
+            _height = height;
+        }
+
+        public HexMap<int> GenerateHeightMap(int mapSize)
+        {
+            var map = new HexMap<int>(mapSize);
             // TODO: Fix duplicate code
             for (var aPos = 0; aPos < mapSize * 2 - 1; aPos++)
             {
                 for (var bPos = 0; bPos < mapSize * 2 - 1; bPos++)
                 {
                     if (Math.Abs(aPos - bPos) >= mapSize) continue;
-                    Map[new HexPos(aPos, bPos)] = 0;
+                    map[new HexPos(aPos, bPos)] = _height;
                 }
             }
+            return map;
         }
     }
 }
