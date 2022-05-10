895dce2311e3641cdd5288381700257fdf5957a2
FridayItsFridayFriday <104296277+FridayItsFridayFriday@users.noreply.github.com>
2022-05-10 13:49:17 +0000

    Fix broken FlatHeightMap

--- Assets/Scripts/Kernel/HeightMaps/FlatHeightMap.cs
+++ Assets/Scripts/Kernel/HeightMaps/FlatHeightMap.cs
@@ -1,9 +1,21 @@
+using System;
+using Utils;
+
 ﻿namespace Kernel.HeightMaps
 {
     public class FlatHeightMap : HeightMap
     {
         public FlatHeightMap(int mapSize) : base(mapSize)
         {
+            // TODO: Fix duplicate code
+            for (var aPos = 0; aPos < mapSize * 2 - 1; aPos++)
+            {
+                for (var bPos = 0; bPos < mapSize * 2 - 1; bPos++)
+                {
+                    if (Math.Abs(aPos - bPos) >= mapSize) continue;
+                    Map[new HexPos(aPos, bPos)] = 0;
+                }
+            }
         }
     }
 }
