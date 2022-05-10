5751e2103f744a68f938145ddddcccbebc64cc0a
FridayItsFridayFriday <104296277+FridayItsFridayFriday@users.noreply.github.com>
2022-05-10 15:17:19 +0000

    Add minHeight parameter for PerlinHeightMapGenerator

--- Assets/Scripts/Kernel/HeightMaps/PerlinHeightMapGenerator.cs
+++ Assets/Scripts/Kernel/HeightMaps/PerlinHeightMapGenerator.cs
@@ -8,12 +8,13 @@ namespace Kernel.HeightMaps
     public class PerlinHeightMapGenerator
     {
         private readonly float _smoothness;
+        private readonly int _minHeight;
         private readonly int _heightVariation;
 
         private readonly int _noiseShiftMin;
         private readonly int _noiseShiftMax;
 
-        public PerlinHeightMapGenerator(float smoothness, int heightVariation)
+        public PerlinHeightMapGenerator(float smoothness, int minHeight, int heightVariation)
         {
             if (smoothness <= 0)
             {
@@ -24,6 +25,7 @@ namespace Kernel.HeightMaps
                 throw new ArgumentOutOfRangeException("heightVariation should be positive.");
             }
             _smoothness = smoothness;
+            _minHeight = minHeight;
             _heightVariation = heightVariation;
             _noiseShiftMin = 10000;
             _noiseShiftMax = 100000;
@@ -40,12 +42,17 @@ namespace Kernel.HeightMaps
                     if (Math.Abs(i - j) >= mapSize) continue;
                     var aPos = i - mapSize + 1;
                     var bPos = j - mapSize + 1;
-                    var height = (int)(Mathf.PerlinNoise(aPos / _smoothness + noiseShift,
-                        bPos / _smoothness + noiseShift) * _heightVariation);
-                    if (height >= _heightVariation)
-                        height = _heightVariation - 1;
-                    else if (height < 0)
-                        height = 0;
+                    var noiseValue = Mathf.PerlinNoise
+                    (
+                        aPos / _smoothness + noiseShift,
+                        bPos / _smoothness + noiseShift
+                    );
+                    var relativeHeight = (int)(_heightVariation * noiseValue);
+                    if (relativeHeight >= _heightVariation)
+                        relativeHeight = _heightVariation - 1;
+                    else if (relativeHeight < 0)
+                        relativeHeight = 0;
+                    var height = _minHeight + relativeHeight;
                     map[new HexPos(aPos + mapSize - 1, bPos + mapSize - 1)] = height;
                 }
             }
